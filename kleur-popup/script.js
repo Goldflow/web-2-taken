640c07565422adf595201735d5ac5fd33da872f5
Karim Dehbi <karim.dehbi@gmail.com>
2022-02-01 15:32:03 +0100

    update

--- kleur-popup/script.js
+++ kleur-popup/script.js
@@ -3,7 +3,7 @@
 // een woord ingeven (aka string)
 var bodyColor = prompt("geef me kleur voor de body"); // we zetten woord gekregen van de gebruiker in de variabele color (zie beetje als lege doos)
 // we gaan een nieuwe variabele declareren genoemd bodyElement
-// en daarin gaan we het HTML-element steken met de naam body
+// en daarin gaan we het HTML-element steken met de naam "body"
 var bodyElement = document.getElementsByTagName("body")[0];
 // dit stuk code gaat van het body element, de style opvragen (bodyElement.style)
 // vervolgens gaat dit de backgroundColor van het body element aanpassen
--- print-lijst/script.js
+++ print-lijst/script.js
@@ -1,3 +1,16 @@
 // dus we maken een lijst van op zijn minst 3 mensen (artiesten, voetballers of filmsterren)
-// elk object met 3 key's hebben (dus 3 verschillende eigenschappen)
+// elk object met 3 key's hebben (dus 3 verschillende relevante eigenschappen)
+// bv naam, leeftijd, genre voor artiest, of naam, leeftijd, ploeg voor voetballer
 // print elke artiest uit in de console
+var objectVoorbeeld1 = { eigenSchap: "hallo", soort: "object" };
+// als je verschillende objecten maakt, zorg dat elk een andere naam heeft
+var objectVoorbeeld2 = { eigenSchap: "hallo", soort: "object" };
+// lijst maken van alle objecten (maar moeten er 3 zijn dus dit is niet goed)
+var lijstObjecten = [objectVoorbeeld1, objectVoorbeeld2];
+// alle objecten uitprinten - druk op F12 om developer tools te zien,
+for (let object of lijstObjecten) {
+    // afhankelijk van als je artiest of voetballer hebt gekozen, kan je hier ofwel
+    // let artiest of lijstArtiesten zetten of let voetballer of lijstVoetballers
+    console.log(object);
+}
+// het woord object wil ik niet terugvinden in jullie code
