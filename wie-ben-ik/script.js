98c595a808225676b513bbd76eaa081c79c9a702
Karim Dehbi <karim.dehbi@gmail.com>
2022-02-01 16:47:32 +0100

    feat: update wie ben ik oefening

---
+++ wie-ben-ik/script.js
@@ -0,0 +1,9 @@
+'use strict';
+// typ "log" om automatisch console.log te laten genereren
+console.log("Karim oef werkt");
+// we gaan de h1 selecteren en dan de achtergrondkleur veranderen
+// h1Element (camelCase notatie)
+let h1Element = document.querySelector('.h1');
+console.log(h1Element);
+// we geven h1Element een nieuwe achtergrondkleur
+h1Element.style.backgroundColor = 'blue';
