7fe43b8a5bd0231b301b7af164c45745203b5809
Karim Dehbi <karim.dehbi@gmail.com>
2022-02-08 14:10:35 +0100

    stap 1 van wiskunde taak

---
+++ wiskunde/script.js
@@ -0,0 +1,8 @@
+// alert gaat een popup tonen met een bericht
+// ik wil in getal 1, het getal ztten dat de gebruik kiest voor getal 1
+let getal1 = prompt('geef het eerste getal');
+// ik wil in getal 2, het getal ztten dat de gebruik kiest voor getal 2
+let getal2 = prompt('geef het 2e getal');
+// getal1 en getal2 zijn een string geworden
+let som = (getal1*1) + (getal2*1); // de som van de getallen hierboven die we gekregen hebben van de gebruiker
+alert(som);
