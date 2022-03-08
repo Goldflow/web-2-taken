6fa04445dcd9bf7d6f2ee88ce632c220ea1aa7e7
Karim Dehbi <karim.dehbi@gmail.com>
2022-03-08 16:26:51 +0100

    feat: upload functie

--- functie/script.js
+++ functie/script.js
@@ -2,23 +2,26 @@ let objectVoorbeeld1 = { eigenSchap: "hallo", soort: "object 1" };
 let objectVoorbeeld2 = { eigenSchap: "hi", soort: "object 2" };
 let objectVoorbeeld3 = { eigenSchap: "hey", soort: "object 3" };
 let lijstObjecten = [objectVoorbeeld1, objectVoorbeeld2, objectVoorbeeld3];
-
-// voorbeeld van loop
-// for (let i = 0; i < 5; i++) {
-//    console.log(i);
-// }
-
 // print hier aantal elementen uit in je array
 console.log(lijstObjecten.length);
 
 // loop, die evenveel keer wordt uitgeprint als elementen in je lijst
 // print het eerste element van de lijst uit
-for (let i = 0; i < lijstObjecten.length; i++) {
-    console.log(lijstObjecten[i]);    
-}
-
 // pas de loop aan zodat je elk element 1 keer uitprint
+for (let i = 0; i < lijstObjecten.length; i++) {
+    objectPrinten(lijstObjecten[i]);
+}
 
 // schrijf een functie die elke eigenschap/property van je object zal uitprinten
 // vb.
+function objectPrinten(parameter) {
+    console.log(parameter.eigenSchap + ' ' + parameter.soort);
+}
 
+// we roepen de functie aan
+// objectPrinten(objectVoorbeeld1)
+/*
+hallo object 1
+hi object 2
+hey object 3
+*/
